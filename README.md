1ed8a2c73f36abbab037cbef523dea97a0dc33e7
qastacker <qastacker@gmail.com>
2020-09-15 17:09:50 +0530

    deleted all files

--- README.md
+++
@@ -1,4 +0,0 @@
-# WebDriverIO
-This file comprises a test script for automating in WebdriverIO
-How to use getText(), sendKeys() and click() methods
-How to run test 
--- test/header.js
+++
@@ -1,9 +0,0 @@
-describe("interaction with web element", function(){
-
-    it("Get header value in field",function(){
-    browser.url('https://www.freshworks.com/');
-    const header = $('h1');
-    let text = header.getText();
-    console.log(text);
-    });
-});
