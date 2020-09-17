ebf35e8d81c8328d7bba4c6917530a73037eb4b2
qastacker <qastacker@gmail.com>
2020-09-17 11:32:09 +0530

    How to handle checkbox and Chai assertions in WebDriverIO

---
+++ tutorials_6/test/checkbox.test.js
@@ -0,0 +1,47 @@
+const intenetHomePage = require("../pages/internetpage");
+const internetPage = require("../pages/internetpage");
+const { expect } = require("chai");
+
+describe("Handle checkbox", function(){
+
+    it("Check the base url", function(){
+        browser.url("/");
+        browser.maximizeWindow();
+        //expect(browser.getUrl()).equals("https://the-internet.herokuapp.com/");
+        expect(browser.getUrl()).equals(`${browser.options.baseUrl}/`);
+    })
+
+    it("Check the checkbox page header", function(){
+        browser.url("/");
+        browser.maximizeWindow();
+        browser.pause('5000');
+        internetPage.clickOnLink(6);
+        browser.pause('5000');
+        internetPage.header.waitForDisplayed();
+        const text = internetPage.header.getText();
+        expect(text).equals("Checkboxes");
+    })
+
+    it("should click on checkbx", function(){
+ 
+        browser.url('/');
+        browser.maximizeWindow();
+        browser.pause('5000');
+        internetPage.clickOnLink(6);
+        browser.pause('5000');
+        internetPage.header.waitForDisplayed();
+        internetPage.clickOnCheckbox(1);
+
+        expect(internetPage.getCheckBoxElement(1).isSelected()).equals(true);  //assertion
+        browser.pause('5000');
+    })
+
+    it("Check the checkbox page url", function(){
+        browser.url("/");
+        browser.maximizeWindow();
+        browser.pause('5000');
+        internetPage.clickOnLink(6);
+        browser.pause('5000');
+        expect(browser.getUrl()).equals(`${browser.options.baseUrl}/checkboxes`);
+    })
+})
