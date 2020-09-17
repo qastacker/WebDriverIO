24b9332205228d61af41a7ac611a66b45c4a6b58
qastacker <qastacker@gmail.com>
2020-09-17 13:18:07 +0530

    How to automate login page - setValue & clearValue in WebDriverIO

---
+++ tutorials_7/README.md
@@ -0,0 +1,4 @@
+# WebDriverIO
+This file comprises a test script for automating in WebdriverIO
+How to automate login page - setValue & clearValue in WebDriverIO
+How to run test 
---
+++ tutorials_7/test/login.test.js
@@ -0,0 +1,28 @@
+const loginPage = require("../pages/loginpage");
+const { expect, assert } = require("chai");
+
+describe("Handle application login", function(){
+
+    it("Enter username", function(){
+        browser.url(`${browser.options.baseUrl}/login`);
+        browser.maximizeWindow();
+        loginPage.enterUserName('tomsmith');
+        assert.equal('tomsmith',loginPage.username.getValue());    
+    });
+
+    it("Enter Password", function(){
+        loginPage.enterUserPassword('SuperSecretPassword');
+        assert.equal('SuperSecretPassword',loginPage.password.getValue());    
+    });
+
+    it("Click login button", function(){
+        loginPage.clickOnLogin();
+    });
+
+    it("should clear username", function(){
+        loginPage.username.click();
+        loginPage.username.clearValue();
+        assert.equal('',loginPage.username.getValue());
+
+    });
+});
